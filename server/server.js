a3b751a6fbb9fc90cde33d70c2d95ea8bf6b8e35
HedleyBenaiges <hedleybenaiges@gmail.com>
2025-05-23 00:59:02 +0100

    test

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader(
+    'Content-Security-Policy', "default-src 'self';"
+  )
   next();
 })
 
